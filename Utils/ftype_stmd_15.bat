12b3447092bd1d87e34a8e734122983854201318
Douglas Hemken <dehemken@wisc.edu>
2019-09-30 15:14:41 -0500

    add filetype utilities

---
+++ Utils/ftype_stmd_15.bat
@@ -0,0 +1,2 @@
+assoc .stmd = stata
+ftype stata "c:\program files (x86)\stata15\stata.exe" -doedit %1
---
+++ Utils/ftype_stmd_16.bat
@@ -0,0 +1,3 @@
+assoc .stmd = Stata16Markdown
+assoc .dyn = Stata16Markdown
+ftype Stata16Markdowntata C:\Program Files\Stata16\StataSE-64.exe" -doedit %1
